6314e21c7c30561c17e466c8b5e56a0537aab7dc
Michael Matuzak <mmatuzak@gmail.com>
2013-12-03 20:06:34 -0800

    moving palette data

--- browser/main.js
+++ browser/main.js
@@ -6,7 +6,6 @@ domready(function () {
     var spriteEditor = document.querySelector('#sprite-editor');
     var fileDrop= document.querySelector('.file-drop');
 
-    console.log(fileDrop);
     fileDrop.ondragover = function () {
         this.className = 'pure-u-1 file-drop hover';
         return false;
@@ -17,29 +16,6 @@ domready(function () {
         return false;
     };
 
-var palette = [
-    0x788084, 0x0000fc, 0x0000c4, 0x4028c4,
-    0x94008c, 0xac0028, 0xac1000, 0x8c1800,
-    0x503000, 0x007800, 0x006800, 0x005800,
-    0x004058, 0x000000, 0x000000, 0x000008,
-
-    0xbcc0c4, 0x0078fc, 0x0088fc, 0x6848fc,
-    0xdc00d4, 0xe40060, 0xfc3800, 0xe46918,
-    0xac8000, 0x00b800, 0x00a800, 0x00a848,
-    0x008894, 0x2c2c2c, 0x000000, 0x000000,
-
-    0xfcf8fc, 0x38c0fc, 0x6888fc, 0x9c78fc,
-    0xfc78fc, 0xfc589c, 0xfc7858, 0xfca048,
-    0xfcb800, 0xbcf818, 0x58d858, 0x58f89c,
-    0x00e8e4, 0x606060, 0x000000, 0x000000,
-
-    0xfcf8fc, 0xa4e8fc, 0xbcb8fc, 0xdcb8fc,
-    0xfcb8fc, 0xf4c0e0, 0xf4d0b4, 0xfce0b4,
-    0xfcd884, 0xdcf878, 0xb8f878, 0xb0f0d8,
-    0x00f8fc, 0xc8c0c0, 0x000000, 0x000000
-];
-
-
     fileDrop.ondrop = function (e) {
         this.className = '';
         e.preventDefault();
@@ -48,7 +24,6 @@ var palette = [
         reader = new FileReader();
         reader.onload = function (event) {
             var result = event.target.result;
-            console.log(result);
 
             var sprites = neslySprite.load(result);
             //console.log(sprites);
@@ -57,9 +32,9 @@ var palette = [
             var context = spriteEditor.getContext('2d');
             var spriteSize = 256;
             var imageData = context.getImageData(0, 0, spriteSize, spriteSize);
-            var sprite = neslySprite.get(0, sprites);
-            newImageData = fillCanvas(sprite, imageData.data, palette, 32, 0);
-            context.putImageData(newImageData, 0, 0);
+            var sprite = neslySprite.get(100, sprites);
+            imageData.data = newImageData = fillCanvas(sprite, imageData.data, 32, 0);
+            context.putImageData(imageData, 0, 0);
         };
         reader.readAsDataURL(file);
 
@@ -67,7 +42,29 @@ var palette = [
     };
 });
 
-function fillCanvas(sprite, imageData, palette, size, padding){
+function fillCanvas(sprite, imageData, size, padding){
+
+    var palette = [
+        0x788084, 0x0000fc, 0x0000c4, 0x4028c4,
+        0x94008c, 0xac0028, 0xac1000, 0x8c1800,
+        0x503000, 0x007800, 0x006800, 0x005800,
+        0x004058, 0x000000, 0x000000, 0x000008,
+
+        0xbcc0c4, 0x0078fc, 0x0088fc, 0x6848fc,
+        0xdc00d4, 0xe40060, 0xfc3800, 0xe46918,
+        0xac8000, 0x00b800, 0x00a800, 0x00a848,
+        0x008894, 0x2c2c2c, 0x000000, 0x000000,
+
+        0xfcf8fc, 0x38c0fc, 0x6888fc, 0x9c78fc,
+        0xfc78fc, 0xfc589c, 0xfc7858, 0xfca048,
+        0xfcb800, 0xbcf818, 0x58d858, 0x58f89c,
+        0x00e8e4, 0x606060, 0x000000, 0x000000,
+
+        0xfcf8fc, 0xa4e8fc, 0xbcb8fc, 0xdcb8fc,
+        0xfcb8fc, 0xf4c0e0, 0xf4d0b4, 0xfce0b4,
+        0xfcd884, 0xdcf878, 0xb8f878, 0xb0f0d8,
+        0x00f8fc, 0xc8c0c0, 0x000000, 0x000000
+    ];
 
     var getColor = function (index) {
         return palette[index];
@@ -81,8 +78,8 @@ function fillCanvas(sprite, imageData, palette, size, padding){
         for (var x=0; x < 8*size; x++) {
             var px = (x/size) >> 0;
             var py = (y/size) >> 0;
-            var color_index = palette[sprt[py][px]];
-            var color = getColor(color_index);
+            var colorIndex = palette[sprite[py][px]];
+            var color = getColor(colorIndex);
             imageData[a] = (color >> 16) & 0xff;
             imageData[a+1] = (color >> 8) & 0xff;
             imageData[a+2] = color & 0xff;
